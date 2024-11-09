dd2f357b07e0deb8b62d21112496aa8830f836e6
Your Name <raktimchatterjee@Raktims-MacBook-Air.local>
2024-11-10 04:59:53 +0530

    Initial commit

--- next.config.js
+++ next.config.js
@@ -2,19 +2,17 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  experimental: {
-    appDir: true,
+  images: {
+    unoptimized: true,
   },
   webpack: (config, { isServer }) => {
     if (!isServer) {
-      // Avoid bundling undici on the client side
       config.resolve.alias = {
         ...config.resolve.alias,
         undici: false,
       };
     }
 
-    // Add babel-loader for handling private class fields
     config.module.rules.push({
       test: /\.m?js$/,
       exclude: /node_modules\/(?!undici)/,
